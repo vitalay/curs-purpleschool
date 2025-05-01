c4909a5511d2bc7a0aeea0d896d8aff8d06a4628
vitalya <vitalay.efimov@gmail.com>
2025-05-01 19:58:51 +0500

    js в шаблонах

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-const color = "dark";
+const id = "red";
+const isAdmin = true;
 </script>
 
 <template>
   <header>logo</header>
-  <main :color>kontent</main>
-
+  <header :class="`color- + ${id}`">Лoro</header>
+  <main :id>KoHTеHT</main>
+  <div>{{ isAdmin ? "Админ" : "Не aдмин" }}</div>
+  <footer>{{ 10 + 20 }}</footer>
 </template>
 
 <style scoped></style>
