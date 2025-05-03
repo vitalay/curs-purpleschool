e284cea57cdb0effdeadea4de96ba6566103829c
vitalya <vitalay.efimov@gmail.com>
2025-05-03 19:03:00 +0500

    slot

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
+import Button from "./components/Button.vue";
+
+
 
 </script>
 
 <template>
   <header>logo</header>
-
+  <Button>
+    <template #icon>+</template>
+    <template #text>-</template>
+    Сохранить</Button>
   <main>Контент</main>
-  
+
   <footer>Подвал</footer>
+
 </template>
 
 <style scoped></style>
---
+++ vue-app/src/components/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <button class="button">
+    <div>
+      <slot name="icon" />
+      <slot name="text" />
+    </div>
+
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+ .button {
+  border: none;
+  background: var(--gradient);
+  font-size: 20px;
+  font-weight: 600;
+  border-radius: 10px;
+  padding: 14px;
+  font-family: var(--font);
+  color: var(--primary);
+  cursor: pointer;
+
+ }
+ .button:hover {
+  background: var(--gradient-inverted);
+ }
+</style>
